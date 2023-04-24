a7d495f7f34f1e40ea3dc83cdab39109e863d905
Luke Sheppard <sheppiegit@gmail.com>
2023-04-24 12:56:02 -0400

    Updated to have the storagefactory contract store and retrieve from the array the numbers stored in each simple storage contract.

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -12,4 +12,17 @@ contract StorageFactory{
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //address
+        //Application binary interface (ABI)
+
+        SimpleStorage simpleStorage = SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex)public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
 }
