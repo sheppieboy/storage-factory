614672a7da71536564c8af4277c2f7027b0873b8
Luke Sheppard <sheppiegit@gmail.com>
2023-04-24 13:04:22 -0400

    Added an Extra Storage contract that inherits from simple storage.

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -8,7 +8,7 @@ contract StorageFactory{
     SimpleStorage[] public simpleStorageArray;
 
     //a function (that anyone can call) to deploy a simple storage contract
-    function createSimpleStorageContract() public {
+    function createSimpleStorageContract() public virtual{
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
